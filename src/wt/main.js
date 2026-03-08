657cd828e4beaaac870f089f243500bd15167cdd
Ihor Stanovyi <ihor.stanovyi@gmail.com>
2026-03-09 01:49:19 +0200

    feat: implement wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,62 @@
+import { readFile } from "fs/promises";
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const directoryName = dirname(fileURLToPath(import.meta.url));
+
 const main = async () => {
-  // Write your code here
-  // Read data.json containing array of numbers
-  // Split into N chunks (N = CPU cores)
-  // Create N workers, send one chunk to each
-  // Collect sorted chunks
-  // Merge using k-way merge algorithm
-  // Log final sorted array
+  const raw = await readFile(join(directoryName, "data.json"), "utf-8");
+  const numbers = JSON.parse(raw);
+  const coreCount = cpus().length;
+
+  const chunkSize = Math.ceil(numbers.length / coreCount);
+  const chunks = [];
+  for (let i = 0; i < coreCount; i++) {
+    chunks.push(numbers.slice(i * chunkSize, (i + 1) * chunkSize));
+  }
+
+  const sortedChunks = await Promise.all(
+    chunks.map(
+      (chunk, i) =>
+        new Promise((resolve, reject) => {
+          const worker = new Worker(join(directoryName, "worker.js"));
+
+          worker.postMessage(chunk);
+
+          worker.on("message", resolve);
+          worker.on("error", reject);
+        }),
+    ),
+  );
+
+  const result = [];
+
+  const pointers = new Array(sortedChunks.length).fill(0);
+
+  while (true) {
+    let minVal = Infinity;
+    let minIdx = -1;
+
+    for (let i = 0; i < sortedChunks.length; i++) {
+      if (pointers[i] < sortedChunks[i].length) {
+        if (sortedChunks[i][pointers[i]] < minVal) {
+          minVal = sortedChunks[i][pointers[i]];
+          minIdx = i;
+        }
+      }
+    }
+
+    if (minIdx === -1) {
+      break;
+    }
+
+    result.push(minVal);
+    pointers[minIdx]++;
+  }
+
+  console.log(result);
 };
 
 await main();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,9 +1,6 @@
-import { parentPort } from 'worker_threads';
+import { parentPort } from "worker_threads";
 
-// Receive array from main thread
-// Sort in ascending order
-// Send back to main thread
-
-parentPort.on('message', (data) => {
-  // Write your code here
+parentPort.on("message", (data) => {
+  const sortedData = [...data].sort((a, b) => a - b);
+  parentPort.postMessage(sortedData);
 });
